baa0a0b5b95b9d49895655cb060f39d03eb3f260
Kent Lui <kuenmao@gmail.com>
2012-09-30 22:44:38 +0800

    Introduce constants file

---
+++ config.h
@@ -0,0 +1,21 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define RAYTRACER_NAMESPACE raytracer
+#define BEGIN_RAYTRACER namespace RAYTRACER {
+#define END_RAYTRACER }
+
+
+BEGIN_RAYTRACER
+
+
+// Single precision color
+typedef float C_FLT;
+
+// Double precision position
+typedef double P_FLT;
+
+
+END_RAYTRACER
+
+#endif
