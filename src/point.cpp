8116ba8c38b951b83e2019fb2216dba0c1be27ee
Kent Lui <kuenmao@gmail.com>
2012-10-02 23:33:28 +0800

    Remove point inheritance from vector

--- src/point.cpp
+++ src/point.cpp
@@ -9,50 +9,44 @@ Point& Point::operator =(const Point &point) {
     return *this;
   }
 
-  this[0] = point[0];
-  this[1] = point[1];
-  this[2] = point[2];
+  x = point.x;
+  y = point.y;
+  z = point.z;
 
   return *this;
 }
 
 
-Point& Point::operator +(const Vector &vec) {
-  P_FLT x = this[0] + vec[0];
-  P_FLT y = this[1] + vec[1];
-  P_FLT z = this[2] + vec[2];
-
-  return Point(x, y, z);
+Point Point::operator +(const Vector &vector) {
+  return Point(x + vector.x, y + vector.y, z + vector.z);
 }
 
 
-Point& Point::operator -(const Vector &vec) {
-  P_FLT x = this[0] - vec[0];
-  P_FLT y = this[1] - vec[1];
-  P_FLT z = this[2] - vec[2];
-
-  return Point(x, y, z);
+Point Point::operator -(const Vector &vector) {
+  return Point(x - vector.x, y - vector.y, z - vector.z);
 }
 
 
-Point& Point::operator +=(const Vector &vec) {
-  this[0] += vec[0];
-  this[1] += vec[1];
-  this[2] += vec[2];
+Point& Point::operator +=(const Vector &vector) {
+  x += vector.x;
+  y += vector.y;
+  z += vector.z;
+
   return *this;
 }
 
 
-Point& Point::operator -=(const Vector &vec) {
-  this[0] -= vec[0];
-  this[1] -= vec[1];
-  this[2] -= vec[2];
+Point& Point::operator -=(const Vector &vector) {
+  x -= vector.x;
+  y -= vector.y;
+  z -= vector.z;
+
   return *this;
 }
 
 
 Point Point::copy() {
-  return Point(this[0], this[1], this[2]);
+  return Point(x, y, z);
 }
 
 
--- src/point.h
+++ src/point.h
@@ -7,19 +7,23 @@
 
 BEGIN_RAYTRACER
 
-class Point::Vector {
+class Point {
   public:
-    Point(P_FLT x, P_FLT y, P_FLT z):Vector(x, y, z);
+    P_FLT x, y, z
 
-    Point(Vector vec):Point(vec);
+    Point(): x(0.0), y(0.0), z(0.0) {}
+    Point(P_FLT x, P_FLT y, P_FLT z): x(x), y(y), z(z) {}
+
+    Point(Vector vector): x(vector.x), y(vector.y), z(vector.z) {}
 
     Point& operator =(const Point &vec);
-    Point& operator +(const Vector &vec);
-    Point& operator -(const Vector &vec);
+
+    Point operator +(const Vector &vec);
+    Point operator -(const Vector &vec);
     Point& operator +=(const Vector &vec);
     Point& operator -=(const Vector &vec);
 
-    Point& copy();
+    Point copy();
 }:
 
 
