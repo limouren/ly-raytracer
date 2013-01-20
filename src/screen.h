96b19f1829046c8351965dbf09f0e0fa4dbe8ab6
Kent Lui <kuenmao@gmail.com>
2013-01-20 20:25:50 +0800

    Cleanup

--- src/screen.h
+++ src/screen.h
@@ -12,7 +12,7 @@
 #include "point.h"
 #include "ray.h"
 #include "solid.h"
-#include "trace.h"
+#include "trace.cpp"
 #include "vector.h"
 
 
@@ -28,7 +28,7 @@ class Screen {
     Screen(Solid scene, Camera camera): scene(scene), camera(camera) {
       int height = 480;
       int width = 640;
-      bitmap_image image(height, width);
+      bitmap_image image(width, height);
 
       Vector dir = camera.target - camera.viewpoint;
       P_FLT horizontal_mag = dir.length() * cos(camera.angle * 0.5);
@@ -45,13 +45,21 @@ class Screen {
       Point top_left = center + top - right;
       Vector i_step = right / (float)(width / 2);
       Vector j_step = top / (float)(height / 2) * -1.0;
+      Vector top_left_pixel = top_left - camera.viewpoint +
+                              (i_step * 0.5) + (j_step * 0.5);
 
       for (int i = 0;i < width;i++) {
         for (int j = 0;j < height;j++) {
-          Vector ray_dir = dir + (i_step * i) + (j_step * j);
+          Vector ray_dir = top_left_pixel + (i_step * i) + (j_step * j);
           Ray ray(camera.viewpoint, ray_dir);
 
-          Color pixel_color = trace(ray);
+          Color c = trace(ray);
+
+          int r = int(c.r * 255 + 0.5);
+          int g = int(c.g * 255 + 0.5);
+          int b = int(c.b * 255 + 0.5);
+
+          image.set_pixel(i, j, r, g, b);
         }
       }
 
--- src/trace.cpp
+++ src/trace.cpp
@@ -1,1 +1,22 @@
-#include "trace.h"
+#ifndef TRACE_CPP
+#define TRACE_CPP
+
+#include "config.h"
+
+#include "color.h"
+#include "ray.h"
+
+
+BEGIN_RAYTRACER
+
+
+Color trace(Ray ray) {
+  Color color(0.5, 0.5, 0.5);
+  return color;
+}
+
+
+END_RAYTRACER
+
+
+#endif
