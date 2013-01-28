08c95a7f5b0d088adb6af6c2b963408e9e5e0a07
Kent Lui <kuenmao@gmail.com>
2013-01-28 23:25:23 +0800

    Aesthetics/linting

--- src/background.h
+++ src/background.h
@@ -45,7 +45,7 @@ class Background {
       C_FLT * blue_channel = new C_FLT[pixel_count];
 
       image.export_rgb(red_channel, green_channel, blue_channel);
-      for (int i = 0;i < pixel_count;i++) {
+      for (int i = 0; i < pixel_count; i++) {
         pixels[i] = Color(red_channel[i], green_channel[i], blue_channel[i]);
       }
 
@@ -60,9 +60,9 @@ class Background {
             x_floor = floor(x),
             y_floor = floor(y);
 
-      int l = (int) x_floor,
+      int l = static_cast<int>(x_floor),
           r = l + 1,
-          t = (int) y_floor,
+          t = static_cast<int>(y_floor),
           b = t + 1;
 
       ratio_x = x - x_floor;
