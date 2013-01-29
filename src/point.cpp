bbddb2b1f60d6cc980ea6173faf0decdc7cd55c0
kent <kent@springsightlabs.com>
2013-01-29 18:19:51 +0800

    Introduce Point2D class

--- src/point.cpp
+++ src/point.cpp
@@ -1,7 +1,60 @@
+#include "config.h"
+
 #include "point.h"
 
 
 BEGIN_RAYTRACER
+
+
+Point2D& Point2D::operator =(const Point2D &point) {
+  if (this == &point) {
+    return *this;
+  }
+
+  x = point.x;
+  y = point.y;
+
+  return *this;
+}
+
+
+const Vector2D Point2D::operator -(const Point2D &point) const {
+  return Vector2D(x - point.x, y - point.y);
+}
+
+
+const Point2D Point2D::operator +(const Vector2D &vector) const {
+  Point2D result = *this;
+  result += vector;
+
+  return result;
+}
+
+
+const Point2D Point2D::operator -(const Vector2D &vector) const {
+  Point2D result = *this;
+  result -= vector;
+
+  return result;
+}
+
+
+Point2D& Point2D::operator +=(const Vector2D &vector) {
+  x += vector.x;
+  y += vector.y;
+
+  return *this;
+}
+
+
+Point2D& Point2D::operator -=(const Vector2D &vector) {
+  x -= vector.x;
+  y -= vector.y;
+
+  return *this;
+}
+
+
 
 
 Point3D& Point3D::operator =(const Point3D &point) {
--- src/point.h
+++ src/point.h
@@ -1,11 +1,57 @@
 #ifndef POINT_H
 #define POINT_H
 
+
 #include "config.h"
+
 #include "vector.h"
 
 
 BEGIN_RAYTRACER
+
+
+class Point2D {
+  public:
+    P_FLT x, y, z;
+
+    Point2D(): x(0.0), y(0.0) {}
+
+    Point2D(P_FLT x, P_FLT y): x(x), y(y) {}
+
+    explicit Point2D(Vector2D vector): x(vector.x), y(vector.y) {}
+
+    const P_FLT operator[](const unsigned int index) const {
+      return (&x)[index];  // Referenced from yafaray
+    }
+
+    P_FLT& operator[](const unsigned int index) {
+      return (&x)[index];  // Referenced from yafaray
+    }
+
+    Point2D& operator =(const Point2D &point);
+
+    const Vector2D operator -(const Point2D &point) const;
+
+    const Point2D operator +(const Vector2D &vector) const;
+    const Point2D operator -(const Vector2D &vector) const;
+    Point2D& operator +=(const Vector2D &vector);
+    Point2D& operator -=(const Vector2D &vector);
+};
+
+
+const P_FLT dotProduct(const Point2D &a, const Point2D &b) {
+  return (a.x * b.x) +
+         (a.y * b.y);
+}
+const P_FLT dotProduct(const Point2D &a, const Vector2D &v) {
+  return (a.x * v.x) +
+         (a.y * v.y);
+}
+const P_FLT dotProduct(const Vector2D &u, const Point2D &b) {
+  return (u.x * b.x) +
+         (u.y * b.y);
+}
+
 
 class Point3D {
   public:
