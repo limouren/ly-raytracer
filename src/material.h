3f93565cdacf34b71a59d1a5ad0c765da9de8bbe
Kent Lui <kuenmao@gmail.com>
2013-02-03 16:12:42 +0800

    Update scene and materials to use consolidated color class

--- src/material.h
+++ src/material.h
@@ -20,13 +20,13 @@ class Material {
           refraction,
           shine;
 
-    Coeff ambience,
+    Color ambience,
           diffuse,
           specular,
           transmission;
 
-    Material(std::string name, Coeff ambience, Coeff diffuse, Coeff specular,
-             Coeff transmission, C_FLT refraction, C_FLT shine):
+    Material(std::string name, Color ambience, Color diffuse, Color specular,
+             Color transmission, C_FLT refraction, C_FLT shine):
              name(name), ambience(ambience), diffuse(diffuse),
              specular(specular), transmission(transmission),
              refraction(refraction), shine(shine) {
