6a1eb2376129e5afd0da5b2b706091a926214425
Kent Lui <kuenmao@gmail.com>
2013-02-16 02:11:57 +0800

    Fix zero checking in bounding box intersect

--- src/bounding_volume.cpp
+++ src/bounding_volume.cpp
@@ -20,52 +20,61 @@ using namespace std;
 const bool Box::intersect(const Ray &ray) const {
   P_FLT inv, t1, t2, tNear, tFar;
 
-  if ((fIsZero(ray.dir.x) &&
-       (ray.orig.x < minExt.x || ray.orig.x > maxExt.x)) ||
-      (fIsZero(ray.dir.y) &&
-       (ray.orig.y < minExt.y || ray.orig.y > maxExt.y)) ||
-      (fIsZero(ray.dir.z) &&
-       (ray.orig.z < minExt.z || ray.orig.z > maxExt.z))) {
-    return false;
-  }
-
   tNear = -P_FLT_MAX;
   tFar = P_FLT_MAX;
 
-  inv = 1.0f / ray.dir.x;
-  t1 = (minExt.x - ray.orig.x) * inv;
-  t2 = (maxExt.x - ray.orig.x) * inv;
-  if (t1 > t2) {
-    swap(t1, t2);
-  }
-  tNear = max(t1, tNear);
-  tFar = min(t2, tFar);
-  if (tNear > tFar || tFar < 0.0f) {
-    return false;
-  }
-
-  inv = 1.0f / ray.dir.y;
-  t1 = (minExt.y - ray.orig.y) * inv;
-  t2 = (maxExt.y - ray.orig.y) * inv;
-  if (t1 > t2) {
-    swap(t1, t2);
-  }
-  tNear = max(t1, tNear);
-  tFar = min(t2, tFar);
-  if (tNear > tFar || tFar < 0.0f) {
-    return false;
+  if (fIsZero(ray.dir.x)) {
+    if (ray.orig.x < minExt.x || ray.orig.x > maxExt.x) {
+      return false;
+    }
+  } else {
+    inv = 1.0f / ray.dir.x;
+    t1 = (minExt.x - ray.orig.x) * inv;
+    t2 = (maxExt.x - ray.orig.x) * inv;
+    if (t1 > t2) {
+      swap(t1, t2);
+    }
+    tNear = max(t1, tNear);
+    tFar = min(t2, tFar);
+    if (tNear > tFar || tFar < 0.0f) {
+      return false;
+    }
   }
 
-  inv = 1.0f / ray.dir.z;
-  t1 = (minExt.z - ray.orig.z) * inv;
-  t2 = (maxExt.z - ray.orig.z) * inv;
-  if (t1 > t2) {
-    swap(t1, t2);
+  if (fIsZero(ray.dir.y)) {
+    if (ray.orig.y < minExt.y || ray.orig.y > maxExt.y) {
+      return false;
+    }
+  } else {
+    inv = 1.0f / ray.dir.y;
+    t1 = (minExt.y - ray.orig.y) * inv;
+    t2 = (maxExt.y - ray.orig.y) * inv;
+    if (t1 > t2) {
+      swap(t1, t2);
+    }
+    tNear = max(t1, tNear);
+    tFar = min(t2, tFar);
+    if (tNear > tFar || tFar < 0.0f) {
+      return false;
+    }
   }
-  tNear = max(t1, tNear);
-  tFar = min(t2, tFar);
-  if (tNear > tFar || tFar < 0.0f) {
-    return false;
+
+  if (fIsZero(ray.dir.z)) {
+    if (ray.orig.z < minExt.z || ray.orig.z > maxExt.z) {
+      return false;
+    }
+  } else {
+    inv = 1.0f / ray.dir.z;
+    t1 = (minExt.z - ray.orig.z) * inv;
+    t2 = (maxExt.z - ray.orig.z) * inv;
+    if (t1 > t2) {
+      swap(t1, t2);
+    }
+    tNear = max(t1, tNear);
+    tFar = min(t2, tFar);
+    if (tNear > tFar || tFar < 0.0f) {
+      return false;
+    }
   }
 
   return true;
