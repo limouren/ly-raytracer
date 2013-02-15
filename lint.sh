3835c4bf5e15073edea0b3568dfe0571dfafe88b
kent <kent@springsightlabs.com>
2013-02-15 23:37:10 +0800

    Include main.cpp in lint

--- lint.sh
+++ lint.sh
@@ -1,1 +1,1 @@
-cpplint --filter=-build/include,-legal/copyright,-build/header_guard,-build/namespaces src/* config/* 2>&1 | grep -v 'Done processing'
+cpplint --filter=-build/include,-legal/copyright,-build/header_guard,-build/namespaces src/* config/* 2>&1 main.cpp | grep -v 'Done processing'
