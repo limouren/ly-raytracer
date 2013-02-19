72c1ee4e676978c458d494aab3f9c732d57f66fa
kent <kent@springsightlabs.com>
2013-02-19 10:30:46 +0800

    Update transform class

--- src/transform.h
+++ src/transform.h
@@ -2,6 +2,8 @@
 #define SRC_TRANSFORM_H
 
 
+#include <math.h>
+
 #include "config.h"
 
 #include "vector.h"
@@ -31,61 +33,132 @@ class Matrix4 {
       return const_cast<P_FLT *>(data[i]);
     }
 
+    Matrix4 inverse() {
+    }
+
     friend Vector3D operator *(const Vector3D &vector);
     friend Matrix4 operator +(const Matrix4 &A, const Matrix4&B);
     friend Matrix4 operator -(const Matrix4 &A, const Matrix4&B);
     friend Matrix4 operator *(const Matrix4 &A, const Matrix4&B);
+
+    static Matrix4 Identity() {
+      return Matrix4(1.0f, 0.0f, 0.0f, 0.0f,
+                     0.0f, 1.0f, 0.0f, 0.0f,
+                     0.0f, 0.0f, 1.0f, 0.0f,
+                     0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    static Matrix4 Zero() {
+      return Matrix4(0.0f, 0.0f, 0.0f, 0.0f,
+                     0.0f, 0.0f, 0.0f, 0.0f,
+                     0.0f, 0.0f, 0.0f, 0.0f,
+                     0.0f, 0.0f, 0.0f, 0.0f);
+    }
 };
 
 
-class Transform: Matrix4 {
+class Transform {
+  protected:
+    P_FLT xScale, yScale, zScale,
+          xRotate, yRotate, zRotate,
+          xTranslate, yTranslate, zTranslate;
+
+    Matrix4 * matrix,
+            * inverse;
+
   public:
     Transform() {}
-    Transform(
-      const P_FLT p00, const P_FLT p01, const P_FLT p02, const P_FLT p03,
-      const P_FLT p10, const P_FLT p11, const P_FLT p12, const P_FLT p13,
-      const P_FLT p20, const P_FLT p21, const P_FLT p22, const P_FLT p23,
-      const P_FLT p30, const P_FLT p31, const P_FLT p32, const P_FLT p33):
-      Matrix4(p00, p01, p02, p03,
-              p10, p11, p12, p13,
-              p20, p21, p22, p23,
-              p30, p31, p32, p33) {}
+    Transform(P_FLT xScale, P_FLT yScale, P_FLT zScale,
+              P_FLT xRotate, P_FLT yRotate, P_FLT zRotate,
+              P_FLT xTranslate, P_FLT yTranslate, P_FLT zTranslate):
+      xScale(xScale), yScale(yScale), zScale(zScale),
+      xRotate(xRotate), yRotate(yRotate), zRotate(zRotate),
+      xTranslate(xTranslate), yTranslate(yTranslate), zTranslate(zTranslate),
+      matrix(new Matrix4()), inverse(new Matrix4()) {
+      Matrix4 scale(xScale, 0.0f, 0.0f, 0.0f,
+                    0.0f, yScale, 0.0f, 0.0f,
+                    0.0f, 0.0f, zScale, 0.0f,
+                    0.0f, 0.0f, 0.0f, 1.0f);
 
-    static Transform identity() {
-      return Transform(1.0f, 0.0f, 0.0f, 0.0f,
-                       0.0f, 1.0f, 0.0f, 0.0f,
-                       0.0f, 0.0f, 1.0f, 0.0f,
+      P_FLT cosXRot = cos(xRotate),
+            sinXRot = sin(xRotate),
+            cosYRot = cos(yRotate),
+            sinYRot = sin(yRotate),
+            cosZRot = cos(zRotate),
+            sinZRot = sin(zRotate);
+      Matrix4 rotate(cosYRot * cosZRot,
+                     sinXRot * sinYRot * sinZRot - cosXRot * sinZRot,
+                     sinXRot * sinZRot + cosXRot * sinYRot * cosZRot,
+                     0.0f,
+
+                     cosYRot * sinZRot,
+                     cosXRot * cosZRot + sinXRot * sinYRot * sinZRot,
+                     cosXRot * sinYRot * sinZRot - sinXRot * cosZRot,
+                     0.0f,
+
+                     -sinYRot,
+                     sinXRot * cosYRot,
+                     cosXRot * cosYRot,
+                     0.0f,
+
+                     0.0f, 0.0f, 0.0f, 1.0f);
+
+      Matrix4 translate(1.0f, 0.0f, 0.0f, xTranslate,
+                        0.0f, 1.0f, 0.0f, yTranslate,
+                        0.0f, 0.0f, 1.0f, zTranslate,
+                        0.0f, 0.0f, 0.0f, 1.0f);
+
+      *matrix = translate * rotate * scale;
+
+      Matrix4 invScale(1.0f / xScale, 0.0f, 0.0f, 0.0f,
+                       0.0f, 1.0f / yScale, 0.0f, 0.0f,
+                       0.0f, 0.0f, 1.0f / zScale, 0.0f,
                        0.0f, 0.0f, 0.0f, 1.0f);
+
+      sinXRot = -sinXRot;
+      sinYRot = -sinYRot;
+      sinZRot = -sinZRot;
+      Matrix4 invRotate(cosYRot * cosZRot,
+                        sinXRot * sinYRot * sinZRot - cosXRot * sinZRot,
+                        sinXRot * sinZRot + cosXRot * sinYRot * cosZRot,
+                        0.0f,
+
+                        cosYRot * sinZRot,
+                        cosXRot * cosZRot + sinXRot * sinYRot * sinZRot,
+                        cosXRot * sinYRot * sinZRot - sinXRot * cosZRot,
+                        0.0f,
+
+                        -sinYRot,
+                        sinXRot * cosYRot,
+                        cosXRot * cosYRot,
+                        0.0f,
+
+                        0.0f, 0.0f, 0.0f, 1.0f);
+
+      Matrix4 invTranslate(1.0f, 0.0f, 0.0f, -xTranslate,
+                           0.0f, 1.0f, 0.0f, -yTranslate,
+                           0.0f, 0.0f, 1.0f, -zTranslate,
+                           0.0f, 0.0f, 0.0f, 1.0f);
+
+      *inverse = invScale * invRotate * invTranslate;
     }
 
-    static Transform zero() {
-      return Transform(0.0f, 0.0f, 0.0f, 0.0f,
-                       0.0f, 0.0f, 0.0f, 0.0f,
-                       0.0f, 0.0f, 0.0f, 0.0f,
-                       0.0f, 0.0f, 0.0f, 0.0f);
+    ~Transform() {
+      delete matrix;
+      delete inverse;
     }
-
-    static Transform rotation() {
-      return Transform();
-    }
-
-    static Transform translation(const Vector3D &direction) {
-      return Transform(1.0f, 0.0f, 0.0f, direction.x,
-                       0.0f, 1.0f, 0.0f, direction.y,
-                       0.0f, 0.0f, 1.0f, direction.z,
-                       0.0f, 0.0f, 0.0f, 1.0f);
-    }
-
 
     void apply(Vector3D * vector) const {
-      *vector = Vector3D(data[0][0] * vector->x + data[0][1] * vector->y +
-                         data[0][2] * vector->z + data[0][3],
-                         data[1][0] * vector->x + data[1][1] * vector->y +
-                         data[1][2] * vector->z + data[1][3],
-                         data[2][0] * vector->x + data[2][1] * vector->y +
-                         data[2][2] * vector->z + data[2][3]);
-      *vector *= 1.0f / (data[3][0] * vector->x + data[3][1] * vector->y +
-                         data[3][2] * vector->z + data[3][3]);
+      Vector3D result(*matrix[0][0] * vector->x + *matrix[0][1] * vector->y +
+                      *matrix[0][2] * vector->z + *matrix[0][3],
+                      *matrix[1][0] * vector->x + *matrix[1][1] * vector->y +
+                      *matrix[1][2] * vector->z + *matrix[1][3],
+                      *matrix[2][0] * vector->x + *matrix[2][1] * vector->y +
+                      *matrix[2][2] * vector->z + *matrix[2][3]);
+      result *= 1.0f / (*matrix[3][0] * vector->x + *matrix[3][1] * vector->y +
+                        *matrix[3][2] * vector->z + *matrix[3][3]);
+
+      *vector = result;
     }
 };
 
